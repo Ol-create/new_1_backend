99801fa30ed1b1f79e2800a1a2bd2cc544124692
Ol-create <oluola96@gmail.com>
2023-11-27 20:01:34 +0100

    first commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let a = 4;
+if (a < 50) {
+    a += 20;
+}
+a = 5;
+console.log(a);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+let a: number = 4
+
+if (a < 50) {
+    a += 20;
+}
+
+a = 5
+
+console.log(a)
